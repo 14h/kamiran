37fb3e3d3bce427740448a9c48a3fcf6f3b45557
14h <abdallah.kawji@gmail.com>
2021-07-05 11:32:55 +0200

    fix grid

--- components/grid.js
+++ components/grid.js
@@ -11,7 +11,7 @@ export default function Grid({projects}) {
                 showAutoplayButton: false,
             }
         }}>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 p-20 mt-20">
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-20 p-4 pb-32 mt-20">
                 {
                     projects.map(
                         (project, i) => (
