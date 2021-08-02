2e643c9210bdd87ba94d026420a8f173a7539940
14h <abdallah.kawji@gmail.com>
2021-08-02 10:31:11 +0200

    footer style change

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-accent-1 border-t border-accent-4 fixed bottom-0 w-full">
-            <div className=" pl-1 py-8 flex flex-col lg:flex-row items-start justify-start container mx-auto">
+            <div className=" pl-1 py-8 flex flex-col lg:flex-row items-start justify-start lg:justify-center container mx-auto text-xs">
                 © KAMIRANKHALIL 2021
             </div>
         </footer>
